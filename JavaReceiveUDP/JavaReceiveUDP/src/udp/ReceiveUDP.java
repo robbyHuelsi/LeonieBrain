f504948800c9647fe7923f9fa73ace5d6ebb4390
FelixOstertag <glimmlampe82@gmail.com>
2016-04-01 17:09:05 +0200

    Streamlides UDP Receive
    Removed all unneccesary stuff from the receiving test programm

--- JavaReceiveUDP/JavaReceiveUDP/src/udp/ReceiveUDP.java
+++ JavaReceiveUDP/JavaReceiveUDP/src/udp/ReceiveUDP.java
@@ -10,83 +10,97 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.util.Iterator;
-import java.util.Set;
 
 public class ReceiveUDP
 {
-	private InetAddress host;
-	private int port;
+	private InetAddress myReceiverAdress;
+	private int myReceicerPort;
 
-	// private static String serverChannel = "serverChannel";
-	// private static String channelType = "channelType";
-
-	public ReceiveUDP(InetAddress host, int port)
+	/**
+	 * 
+	 * @param myReceiverAdress
+	 *            the adress of the interface to receive data from (my IP-Adress)
+	 * @param myReceicerPort
+	 *            the port to listen to
+	 */
+	public ReceiveUDP(InetAddress myReceiverAdress, int myReceicerPort)
 	{
 		super();
-		this.host = host;
-		this.port = port;
+		this.myReceiverAdress = myReceiverAdress;
+		this.myReceicerPort = myReceicerPort;
 	}
 
-	public String receive(boolean block) throws Exception
+	/**
+	 * 
+	 * @param block
+	 * @param senderAdress
+	 * @param senderPort
+	 * @return
+	 * @throws Exception
+	 */
+	public String receiveChannel(boolean block, InetAddress senderAdress, int senderPort)
 	{
 		String result = null;
-		DatagramChannel receivingChannel = DatagramChannel.open();
-
-		SocketAddress receivingSocket = new InetSocketAddress(InetAddress.getLoopbackAddress(), 8888);
-		InetSocketAddress sendingSocket = new InetSocketAddress(InetAddress.getLoopbackAddress(), 9999);
-
-		receivingChannel.socket().bind(receivingSocket);
-		receivingChannel.configureBlocking(block);
-		receivingChannel.connect(sendingSocket);
-
-		ByteBuffer packet = ByteBuffer.allocate(1024);
-		packet.clear();
-		System.out.println(((InetSocketAddress) receivingSocket).getAddress() + ":" + ((InetSocketAddress) receivingSocket).getPort());
-
-		if (receivingChannel.receive(packet) != null)
+		try
 		{
-			System.out.println("XXX");
-			result = new String(packet.array());
+			DatagramChannel receivingChannel = DatagramChannel.open();
+
+			SocketAddress receivingSocket = new InetSocketAddress(myReceiverAdress, myReceicerPort);
+			InetSocketAddress sendingSocket = new InetSocketAddress(senderAdress, senderPort);
+
+			receivingChannel.socket().bind(receivingSocket);
+			receivingChannel.configureBlocking(block);
+			receivingChannel.connect(sendingSocket);
+
+			ByteBuffer packet = ByteBuffer.allocate(1024);
+			packet.clear();
+			// System.out.println(((InetSocketAddress) receivingSocket).getAddress() + ":" + ((InetSocketAddress) receivingSocket).getPort());
+
+			if (receivingChannel.receive(packet) != null)
+				result = new String(packet.array());
+
+			receivingChannel.close();
+		} catch (SocketException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		System.out.println(result);
-		receivingChannel.close();
 
 		return result;
 	}
 
-	// public String receive()
-	// {
-	// DatagramPacket request = null;
-	// try
-	// {
-	// DatagramSocket socket = new DatagramSocket(port, host);
-	//
-	// byte[] receiveData = new byte[1024];
-	// request = new DatagramPacket(receiveData, receiveData.length);
-	//
-	// socket.receive(request);
-	//
-	// socket.close();
-	// } catch (UnknownHostException e)
-	// {
-	// System.err.println("Host Adress not found!");
-	// e.printStackTrace();
-	// } catch (SocketException e)
-	// {
-	// System.err.println("Socket creation error!");
-	// e.printStackTrace();
-	// } catch (IOException e)
-	// {
-	// System.err.println("Error on receiving package!");
-	// e.printStackTrace();
-	// }
-	//
-	// return new String(request.getData(), request.getOffset(), request.getLength());
-	// }
+	public String receiveSocket()
+	{
+		DatagramPacket request = null;
+		try
+		{
+			DatagramSocket socket = new DatagramSocket(8884, myReceiverAdress);
+
+			byte[] receiveData = new byte[1024];
+			request = new DatagramPacket(receiveData, receiveData.length);
+
+			socket.receive(request);
+
+			socket.close();
+		} catch (UnknownHostException e)
+		{
+			System.err.println("Host Adress not found!");
+			e.printStackTrace();
+		} catch (SocketException e)
+		{
+			System.err.println("Socket creation error!");
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			System.err.println("Error on receiving package!");
+			e.printStackTrace();
+		}
+
+		return new String(request.getData(), request.getOffset(), request.getLength());
+	}
 
 }
