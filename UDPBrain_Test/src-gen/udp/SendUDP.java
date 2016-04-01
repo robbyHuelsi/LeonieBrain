6abdc2ae429b29d7dda458474440acfd27c02875
FelixOstertag <glimmlampe82@gmail.com>
2016-04-01 17:40:51 +0200

    Merged two sending classes
    merged the 2 sending UDP classes to one, similar to the test programm

--- UDPBrain_Test/src-gen/udp/SendUDP.java
+++ UDPBrain_Test/src-gen/udp/SendUDP.java
@@ -1,6 +1,8 @@
 package udp;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -8,7 +10,7 @@ import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 
-public class SendStringChannel
+public class SendUDP
 {
 	private InetAddress senderAdress;
 	private int senderPort;
@@ -18,7 +20,7 @@ public class SendStringChannel
 	 * @param senderAdress Adress of the interface from which to send the data
 	 * @param senderPort Port from which to send
 	 */
-	public SendStringChannel(InetAddress senderAdress, int senderPort)
+	public SendUDP(InetAddress senderAdress, int senderPort)
 	{
 		this.senderAdress = senderAdress;
 		this.senderPort = senderPort;
@@ -30,7 +32,7 @@ public class SendStringChannel
 	 * @param targetAdress the adress of the target
 	 * @param targetPort the port of the target
 	 */
-	public void send(String text, InetAddress targetAdress, int targetPort)
+	public void sendChannel(String text, InetAddress targetAdress, int targetPort)
 	{
 		SocketAddress receivingSocket = new InetSocketAddress(targetAdress, targetPort);
 		SocketAddress sendingSocket = new InetSocketAddress(senderAdress, senderPort);
@@ -61,5 +63,37 @@ public class SendStringChannel
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * 
+	 * @param message the Message
+	 * @param targetAdress IP adress of the target
+	 * @param targetPort Port of the target
+	 */
+	public void sendSocket(String message, InetAddress targetAdress, int targetPort)
+	{
+		DatagramSocket socket;
+		
+		DatagramPacket request = null;
+		try
+		{
+			socket = new DatagramSocket();
+			byte[] sendData = message.getBytes();
+
+			request = new DatagramPacket(sendData, sendData.length, targetAdress, targetPort);
+			socket.send(request);
+		} catch (SocketException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Packet versendet: " + request.getAddress() + ":" + request.getPort() + " -> "
+				+ new String(request.getData()));
 	}
 }
